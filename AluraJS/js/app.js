ec1613a3ec18303a1ed9c8183023db584f1653ba
jair Santana <jairsantana7@gmail.com>
2021-10-05 18:37:17 -0300

    ok

--- AluraJS/js/app.js
+++ AluraJS/js/app.js
@@ -1,23 +1,88 @@
-let tbpacientes = document.querySelector(".paciente");
-let paciente = document.querySelector(".paciente");
-let tdPeso = paciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
+let tbpacientes = document.querySelectorAll(".paciente");
+let titulo = document.querySelector(".titulo");
+let pessoa;
+let imc = 0;
+//titulo.addEventListener("click", mostrarMsg);
 
-if (peso > 1000 || peso < 0) {
-  console.log("Peso Inválido");
-}
-if (altura > 2000 || altura < 0) {
-  console.log("Altura Inválido");
-}
-let imc = peso / (altura * altura);
-let imcPaciente = paciente.querySelector(".info-imc");
-imcPaciente.innerHTML = imc;
+let buttonAdicionaPaciente = document.querySelector("#adicionar-paciente");
 
-//console.log(imcPaciente);
+// capturar dados
+/*
+<tr class="paciente">
+  <td class="info-nome">Erica</td>
+  <td class="info-peso">54</td>
+  <td class="info-altura">5000</td>
+  <td class="info-gordura">14</td>
+  <td class="info-imc">0</td>
+</tr>;
+*/
 
-for (let index = 0; index < tbpacientes.length; index++) {
-  const element = tbpacientes[index];
-  //console.log(element[index]);
+function cadastrarDados(name, peso, altura, gordura) {
+  this.name = name;
+  this.peso = peso;
+  this.altura = altura;
+  this.gordura = gordura;
 }
+
+console.log(buttonAdicionaPaciente);
+
+//console.log(tbpacientes);
+
+for (let i = 0; i < tbpacientes.length; i++) {
+  let paciente = tbpacientes[i];
+
+  let pesoValido = true;
+  let alturaValido = true;
+
+  let tdPeso = paciente.querySelector(".info-peso");
+  let peso = tdPeso.textContent;
+  let tdAltura = paciente.querySelector(".info-altura");
+  let altura = tdAltura.textContent;
+
+  if (peso >= 1000 || peso <= 0) {
+    let ivalidImc = paciente.querySelector(".info-imc");
+    ivalidImc.textContent = "Peso Inválido";
+    paciente.style.backgroundColor = "red";
+    paciente.classList.add("ivalid");
+    pesoValido = false;
+  }
+  if (altura >= 2000 || altura <= 0) {
+    let ivalidImc = paciente.querySelector(".info-imc");
+    ivalidImc.textContent = "Altura Inválido";
+    paciente.style.backgroundColor = "orange";
+    paciente.classList.add("ivalid");
+    alturaValido = false;
+  }
+
+  if (pesoValido && alturaValido) {
+    let imc = peso / (altura * altura);
+    let imcPaciente = paciente.querySelector(".info-imc");
+    imcPaciente.innerHTML = imc.toFixed(2);
+  }
+}
+
+console.log(imc);
+buttonAdicionaPaciente.addEventListener("click", function () {
+  this.defaultPrevented;
+
+  const name = document.querySelector("#nome").value;
+  const peso = document.querySelector("#peso").value;
+  const altura = document.querySelector("#altura").value;
+  const gordura = document.querySelector("#gordura").value;
+  pessoa = new cadastrarDados(name, peso, altura, gordura);
+  //let novaLinha = document.createElement("tr");
+
+  let novaLinha = document.createElement("tr");
+  novaLinha.classList.add("paciente");
+  novaLinha.innerHTML = `  
+  <td class="info-nome">${name}</td>
+  <td class="info-peso">${peso}</td>
+  <td class="info-altura">${altura}</td>
+  <td class="info-gordura">${gordura}</td>
+  <td class="info-imc">0</td>`;
+  let tota = document.querySelector("#tabela-pacientes");
+  tota.insertAdjacentHTML("afterbegin", novaLinha.innerHTML);
+  //console.log(tota);
+  console.log(novaLinha);
+  console.log(pessoa);
+});
